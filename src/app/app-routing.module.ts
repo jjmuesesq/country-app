62f6807b587c8d92ae46e29a3c9cd0f44d0ee5bb
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-16 12:02:16 -0500

    :sparkles: Solucion a la tarea routing contact-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
   {
@@ -13,8 +14,8 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
-    path: 'about',
-    component: AboutPageComponent
+    path: 'contact',
+    component: ContactPageComponent
   },
   {
     path: '**',
