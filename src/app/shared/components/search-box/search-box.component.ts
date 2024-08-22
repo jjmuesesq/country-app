8e2bef98a751fc68b29f34b31bc19fec876c9d2a
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-21 21:28:19 -0500

    :sparkles: limpieza de suscripciones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,27 +1,28 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   
   //Esperar a que el usuario deje de escribir para lanzar la petición
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
   
   @Input()
   public placeholder: string = '';
   
   @Output() 
   public onValue = new EventEmitter<string>();
-
+  
   @Output() 
   public onDebounce = new EventEmitter<string>();
   
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscription = this.debouncer
     .pipe(
       debounceTime(300)
     )
@@ -30,6 +31,10 @@ export class SearchBoxComponent implements OnInit {
     })
   }
   
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   emitValue( value: string):void {
     this.onValue.emit(value);
   }
