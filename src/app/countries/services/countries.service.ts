2685941d47246876e58b459aedea8a1ebf1f944a
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-22 21:28:01 -0500

    :sparkles: Mantener la data entre pantallas

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, delay, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
 
     private apiUrl:string = 'https://restcountries.com/v3.1';
-    constructor(private http: HttpClient) { }
+
+    public cacheStore: CacheStore = {
+        byCapital:   { term: '', countries: []},
+        byCountries: { term: '', countries: []},
+        byRegion:    { region: '', countries: []}
+    }
+
+    constructor(private http: HttpClient) {}
 
     private getCountriesRequest(url: string): Observable<Country[]>{
         return this.http.get<Country[]>(url)
