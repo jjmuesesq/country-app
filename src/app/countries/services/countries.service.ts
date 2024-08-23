0a4656b8f090275e3827f588dc4bc570055cc8a9
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-22 22:34:40 -0500

    :sparkles: Resolucion de la tarea

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+
 import { Country } from '../interfaces/country';
 import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -44,12 +46,18 @@ export class CountriesService {
 
     searchCountry(term: string): Observable<Country[]>{
         const url = `${this.apiUrl}/name/${term}`;
-        return this.getCountriesRequest(url);
+        return this.getCountriesRequest(url)
+            .pipe(
+                tap( countries => this.cacheStore.byCountries = {term, countries})
+            );
     }
 
-    searchRegion(region: string): Observable<Country[]>{
+    searchRegion(region: Region): Observable<Country[]>{
         const url = `${this.apiUrl}/region/${region}`;
-        return this.getCountriesRequest(url);
+        return this.getCountriesRequest(url)
+            .pipe(
+                tap( countries => this.cacheStore.byRegion = {region, countries})
+            );
     }
     
 }
